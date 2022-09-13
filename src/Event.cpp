5cbd8e55aefa63fa72238696080c398d80ef8580
nevergarden <nevergarden.fa@gmail.com>
2022-09-13 04:53:50 +0430

    add: events in c++

--- src/Event.cpp
+++ src/Event.cpp
@@ -1,9 +1,12 @@
 #include "Event.hpp"
 #include "Types.hpp"
+#include <vector>
 
 using namespace FireZombie::Events;
 
 fz_uint8 const Event::ADDED = 0;
+
+std::vector<Event*> Event::eventPool;
 
 Event* Event::fromPool(fz_uint8 type, fz_void *data) {
   if(Event::eventPool.empty())
@@ -14,22 +17,37 @@ Event* Event::fromPool(fz_uint8 type, fz_void *data) {
   return e;
 }
 
-void Event::reset(fz_uint8 type) {
+fz_void Event::toPool(Event *event) {
+  Event::eventPool.push_back(event);
+}
+
+fz_void Event::reset(fz_uint8 type) {
   this->type = type;
   this->data = fz_null;
 }
 
-void Event::reset(fz_uint8 type, fz_void * data) {
+fz_void Event::reset(fz_uint8 type, fz_void * data) {
   this->type = type;
   this->data = data;
 }
 
+fz_uint8 Event::getType() {
+  return this->type;
+}
+
+fz_void Event::setType(fz_uint8 type) {
+  this->type = type;
+}
+
+fz_void* Event::getData() {
+  return this->data;
+}
+
+fz_void Event::setData(fz_void *data) {
+  this->data = data;
+}
 
 Event::Event(fz_uint8 type, fz_void* data) {
   this->type = type;
   this->data = data;
-}
-
-Event::~Event()
-{
 }
--- src/Event.hpp
+++ src/Event.hpp
@@ -11,15 +11,22 @@ class Event
 public:
     static fz_uint8 const ADDED;
     static Event* fromPool(fz_uint8 type, fz_void* data);
-    ~Event();
+    static fz_void toPool(Event* event);
+    
+    fz_uint8 getType();
+    fz_void setType(fz_uint8 type);
+
+    fz_void* getData();
+    fz_void setData(fz_void* data);
+
 protected:
     fz_uint8 type;
     fz_void* data;
 
 private:
     Event(fz_uint8 type, fz_void* data);
-    void reset(fz_uint8 type);
-    void reset(fz_uint8 type, fz_void* data);
+    fz_void reset(fz_uint8 type);
+    fz_void reset(fz_uint8 type, fz_void* data);
     static std::vector<Event*> eventPool;
 };
 }
